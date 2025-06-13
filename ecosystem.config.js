814f600b4d4101de0d8ef52d1078dcc0c132c137
SinghUdayG <udaysinghg70@gmail.com>
2025-06-13 14:46:40 +0530

    deployment 6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,26 @@ module.exports = {
     name: "websocket-server",
     script: "php",
     args: "websocket_server.php",
-    watch: true,
+    watch: false,
     log_date_format: "YYYY-MM-DD HH:mm:ss",
     error_file: "logs/websocket-error.log",
     out_file: "logs/websocket-out.log",
     merge_logs: true,
-    autorestart: true
+    autorestart: true,
+    max_restarts: 10,
+    min_uptime: "5s"
   },
   {
     name: "mqtt-client",
     script: "php",
     args: "mqtt_client.php",
-    watch: true,
+    watch: false,
     log_date_format: "YYYY-MM-DD HH:mm:ss",
     error_file: "logs/mqtt-error.log",
     out_file: "logs/mqtt-out.log",
     merge_logs: true,
-    autorestart: true
+    autorestart: true,
+    max_restarts: 10,
+    min_uptime: "5s"
   }]
 } 
