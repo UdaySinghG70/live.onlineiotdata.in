e5aba9118a39ea3de168bc5b8a74910b8f167173
SinghUdayG <udaysinghg70@gmail.com>
2025-06-24 17:14:51 +0530

    fixing device detail Xx1

--- admin/devices.php
+++ admin/devices.php
@@ -239,11 +239,12 @@ $userArr=$adao->getAllUsers();
         #rechargeModal {
             display: none;
             position: fixed;
-            top: 0; left: 0; width: 150vw; height: 100vh;
+            top: 0; left: 0; width: 100vw; height: 100vh;
             background: rgba(0,0,0,0);
             z-index: 9999;
             align-items: center;
             justify-content: center;
+            display: flex;
         }
         #rechargeModalContent {
             background: #fff;
